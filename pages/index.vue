0fd03d36402d39bed876bc11a38d79df146e3429
gmyou <gmyou@motiv-i.com>
2025-01-17 16:57:10 +0900

    Feat : 변수 선언 방법, 함수 사용법
    https://velog.io/@nuxt/NUXT-%EB%B3%80%EC%88%98%EC%99%80%ED%95%A8%EC%88%98

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial/>
+  <div @click="foo">
+    a = {{ a }}
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data: () => ({
+    a: 10,
+  }),
+  methods: {
+    foo() {
+      this.a += 1;
+    }
+  }
 }
 </script>
